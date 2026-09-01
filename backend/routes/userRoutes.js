0fa299236adeb2efaa29143e483379a2a960b252
Nishant <bhatinishant38@gmail.com>
2026-09-01 14:19:23 +0530

    feat: implement appointment booking functionality; add bookAppointment API and update userRoutes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { getUserdata, loginUser, registerUser, updatingUserdata } from '../controllers/userController.js'
+import { bookAppointment, getUserdata, loginUser, registerUser, updatingUserdata } from '../controllers/userController.js'
 import { authUser } from '../middleware/userMiddleware.js'
 import { upload } from '../middleware/multer.js'
 
@@ -9,3 +9,4 @@ userRouter.post('/register' ,registerUser)
 userRouter.post('/login' ,loginUser)
 userRouter.get('/get-profile' ,authUser ,getUserdata)
 userRouter.post('/update-profile',authUser,upload.single('image') ,updatingUserdata)
+userRouter.post('/book-appointment',authUser ,bookAppointment)
