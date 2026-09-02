9bb00fd818b0fe519788fa4f2b4f2da532dfd677
Nishant <bhatinishant38@gmail.com>
2026-09-02 14:28:49 +0530

    feat: add API for canceling appointments; implement cancellation logic and update doctor's booked slots

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -184,3 +184,37 @@ export const getAppointmentlist = async (req,res)=>{
     res.json({success: false, message:error.message})    
    }
 }
+
+
+// API to cancel Apponitment
+
+export const cancelAppointment = async (req,res)=>{
+
+  try {
+    const userId = req.userId
+    const {appointmentId} = req.body
+
+    const appointmentData = await appointmentModel.findById(appointmentId)
+
+    // checking that the appointment data is of same user
+    if(appointmentData.userId !== userId){
+      return res.json({success:false ,message : "Unauthorized action"})
+    }
+
+    const {docId ,slotDate ,slotTime} = appointmentData
+
+    const docData = await doctorModel.findById(docId)
+
+    let slots_booked = docData.slots_booked
+
+    slots_booked[slotDate] = slots_booked[slotDate].filter(e=> e!== slotTime)
+
+   await doctorModel.findByIdAndUpdate(docId ,{slots_booked})
+   res.json({success : true ,message : "Appointment canceled"})
+    
+  } catch (error) {
+    console.log(error)
+    res.json({success: false, message:error.message})  
+  }
+
+}
