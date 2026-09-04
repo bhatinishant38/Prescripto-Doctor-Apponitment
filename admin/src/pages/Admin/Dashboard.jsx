e217beb91efa953ba4946c5520d328cf10306329
Nishant <bhatinishant38@gmail.com>
2026-09-05 00:45:48 +0530

    feat: refactor Sidebar and Dashboard components for improved layout and functionality; enhance token handling and data display

--- admin/src/pages/Admin/Dashboard.jsx
+++ admin/src/pages/Admin/Dashboard.jsx
@@ -1,73 +1,82 @@
-import React, { useContext, useEffect } from 'react'
-import { AdminContext } from '../../context/AdminContext'
-import { assets } from '../../assets/assets'
+import React, { useContext, useEffect } from "react";
+import { AdminContext } from "../../context/AdminContext";
+import { assets } from "../../assets/assets";
 
 const Dashboard = () => {
+  const { aToken, getDashData, dashData, cancelAppointment } =
+    useContext(AdminContext);
 
-  const { aToken ,getDashData,dashData ,cancelAppointment} = useContext(AdminContext)
-
-  useEffect(()=>{
-    if(aToken){
-      getDashData()
-
+  useEffect(() => {
+    if (aToken) {
+      getDashData();
     }
+  }, [aToken]);
+  return (
+    dashData && (
+      <div className="m-5">
+        <div className="flex flex-wrap gap-3">
+          <div className="flex items-center gap-2 bg-white p-4 min-w-52 rounded border-2 border-gray-100 cursor-pointer hover:scale-105 transition-all">
+            <img className="w-14" src={assets.doctor_icon} alt="" />
+            <div>
+              <p className="text-xl font-semibold text-gray-600">
+                {dashData.doctors}
+              </p>
+              <p className="text-gray-400">Doctors</p>
+            </div>
+          </div>
 
-  },[aToken])
-  return dashData && (
-    <div className='m-5'>
+          <div className="flex items-center gap-2 bg-white p-4 min-w-52 rounded border-2 border-gray-100 cursor-pointer hover:scale-105 transition-all">
+            <img className="w-14" src={assets.appointments_icon} alt="" />
+            <div>
+              <p className="text-xl font-semibold text-gray-600">
+                {dashData.appointments}
+              </p>
+              <p className="text-gray-400">Appointments</p>
+            </div>
+          </div>
 
-      <div className='flex flex-wrap gap-3'>
-
-        <div className='flex items-center gap-2 bg-white p-4 min-w-52 rounded border-2 border-gray-100 cursor-pointer hover:scale-105 transition-all'>
-          <img className='w-14' src={assets.doctor_icon} alt="" />
-          <div>
-            <p className='text-xl font-semibold text-gray-600'>{dashData.doctors}</p>
-            <p className='text-gray-400'>Doctors</p>
+          <div className="flex items-center gap-2 bg-white p-4 min-w-52 rounded border-2 border-gray-100 cursor-pointer hover:scale-105 transition-all">
+            <img className="w-14" src={assets.patients_icon} alt="" />
+            <div>
+              <p className="text-xl font-semibold text-gray-600">
+                {dashData.patients}
+              </p>
+              <p className="text-gray-400">Patients</p>
+            </div>
           </div>
         </div>
 
-        <div className='flex items-center gap-2 bg-white p-4 min-w-52 rounded border-2 border-gray-100 cursor-pointer hover:scale-105 transition-all'>
-          <img className='w-14' src={assets.appointments_icon} alt="" />
-          <div>
-            <p className='text-xl font-semibold text-gray-600'>{dashData.appointments}</p>
-            <p className='text-gray-400'>Appointments</p>
+        <div className="bg-white">
+          <div className="flex items-center gap-2.5 px-4 py-4 mt-10 rounded-t border border-gray-100">
+            <img src={assets.list_icon} alt="" />
+            <p className="font-semibold">Latest Bookings</p>
           </div>
-        </div>
 
-        <div className='flex items-center gap-2 bg-white p-4 min-w-52 rounded border-2 border-gray-100 cursor-pointer hover:scale-105 transition-all'>
-          <img className='w-14' src={assets.patients_icon} alt="" />
-          <div>
-            <p className='text-xl font-semibold text-gray-600'>{dashData.patients}</p>
-            <p className='text-gray-400'>Patients</p>
-          </div>
-        </div>
-
-      </div>
-
-      <div className='bg-white'>
-
-        <div className='flex items-center gap-2.5 px-4 py-4 mt-10 rounded-t border border-gray-100'>
-          <img src={assets.list_icon} alt="" />
-          <p className='font-semibold'>Latest Bookings</p>
-        </div>
-
-        <div className='pt-4 border border-t-0 border-gray-100'>
-          {
-            dashData.latestAppointments.map((item,index)=>(
+          <div className="pt-4 border border-t-0 border-gray-100">
+            {dashData.latestAppointments.map((item, index) => (
               <div key={index}>
                 <img src={item.docData.image} alt="" />
-
+                <div>
+                  <p>{item.docData.image}</p>
+                  <p>{item.slotDate}</p>
+                </div>
+                {item.cancelled ? (
+                  <p className="text-red-400 text-xs font-medium">cancelled</p>
+                ) : (
+                  <img
+                    onClick={() => cancelAppointment(item._id)}
+                    className="w-10 cursor-pointer"
+                    src={assets.cancel_icon}
+                    alt=""
+                  />
+                )}
               </div>
-
-            ))
-
-          }
-        
+            ))}
+          </div>
         </div>
       </div>
+    )
+  );
+};
 
-    </div>
-  )
-}
-
-export default Dashboard
+export default Dashboard;
